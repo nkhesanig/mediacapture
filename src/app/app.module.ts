e8cb0b7f6a2b49950764d0ab08360dd79e080430
nkhesani <nkhesani.mukoveni@gmail.com>
2018-10-23 11:04:48 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MediaCapture} from '@ionic-native/media-capture';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    MediaCapture, 
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureAudioOptions, CaptureVideoOptions } from '@ionic-native/media-capture';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private mediaCapture: MediaCapture) {
 
+  }
+  video(){
+    let options:CaptureVideoOptions = { limit: 3 };
+  this.mediaCapture.captureVideo(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  }
+
+  pic(){
+    let options: CaptureImageOptions = { limit: 3 };
+    this.mediaCapture.captureImage(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  }
+
+  audio(){
+    let options: CaptureAudioOptions = { limit: 3 };
+     this.mediaCapture.captureAudio(options)
+    .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
   }
 
 }
